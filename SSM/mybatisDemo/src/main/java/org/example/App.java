ed37ca246744d0dabb0d65647d2d04e1752c0b42
sdfiv <hyh8636@outlook.com>
2020-06-27 20:30:59 +0800

    SSM: mybatis demo

---
+++ SSM/mybatisDemo/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
